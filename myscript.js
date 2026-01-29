95f98da8814f688a9abaedaa655c59a4b7a26ae8
ErlinGuillen <guillenvelasquezerlin@gmail.com>
2026-01-29 16:21:25 -0500

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,3 +1,3 @@
 function myFunction() {
-  document.getElementById("demo2").innerHTML = "<a href="https://www.w3schools.com">This is a link</a>"
+  document.getElementById("demo2").innerHTML = "<h1>BOOM!</h1>"
 }
