b742835e72a704760efa3728b6a23bda0dbb60f8
ErlinGuillen <guillenvelasquezerlin@gmail.com>
2026-01-30 14:14:04 -0500

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,4 +1,5 @@
 function myFunction() {
   document.getElementById("demo2").innerHTML = "<h1>BOOM!</h1>"
-  document.getElementById("demo2").
+  document.getElementById("demo").innerHTML = "v0.4";
+  document.getElementById("demo").
 }
