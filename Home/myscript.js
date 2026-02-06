7bfea1b1e6d13a686e6d054ab9f00366ab79c2ed
ErlinGuillen <guillenvelasquezerlin@gmail.com>
2026-02-06 18:37:39 -0500

    Update myscript.js

--- Home/myscript.js
+++ Home/myscript.js
@@ -1,1 +1,12 @@
-
+function showAlert() { 
+  window.alert("This setting is unavaliable (Error: 404)"); 
+}
+function changeText() { 
+  document.getElementById("msg").innerText = "Button clicked!"; 
+}
+function logMessage() { 
+  console.log("Hello, World"); 
+}
+function printPage() { 
+  window.print(); 
+}
