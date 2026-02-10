0b9dba1c77cc4490b84a469ef87b7365fd1b7230
ErlinGuillen <guillenvelasquezerlin@gmail.com>
2026-02-09 21:56:30 -0500

    Update myscript.js

--- Home/myscript.js
+++ Home/myscript.js
@@ -1,51 +1,47 @@
-=// Change text when the button is clicked
-const changeText = () => {
+// Change text when the button is clicked
+function changeText() {
   document.getElementById("msg").innerText = "Button clicked!";
-};
+}
 
 // Log a message to the console
-const logMessage = () => {
+function logMessage() {
   console.log("Hello, World");
-};
+}
 
 // Print the page
-const printPage = () => {
+function printPage() {
   window.print();
-};
+}
 
 // Show heading and paragraph
-const showParagraph = () => {
+function myParagraph() {
   document.getElementById("demo1").innerHTML = "<ins>Information</ins>";
-  document.getElementById("demo2").innerText =
-    "This is currently under construction";
-};
+  document.getElementById("demo2").innerText = "This is currently under construction";
+}
 
 // Count button clicks
-let clicks = 0;
-const addClick = () => {
+var clicks = 0;
+function addClick() {
   clicks++;
   document.getElementById("clicks").innerText = clicks;
-};
+}
 
 // Console info
-const siteName = "Home";
+var siteName = "Home";
 console.log("Site:", siteName);
 
 // Simple calculation
-const count = 1;
-const date = 8;
-const sum = count + date;
+var count = 1;
+var date = 8;
+var sum = count + date;
 console.log(sum);
 
 // Array example
-const subjects = ["Math", "Reading", "Science"];
-
-// Modify array
+var subjects = ["Math", "Reading", "Science"];
 subjects[2] = "Class";
 subjects.push("By Erlin");
 
 // Display array
 document.getElementById("demo3").innerText = subjects.join(", ");
-
 
 
