05862b295887a1505c3f59b6510c2ff7d24ab5a3
ErlinGuillen <guillenvelasquezerlin@gmail.com>
2026-02-09 21:47:39 -0500

    Update myscript.js

--- Home/myscript.js
+++ Home/myscript.js
@@ -1,45 +1,51 @@
-// Show a text when the button is clicked
-function changeText() { 
-  document.getElementById("msg").innerText = "Button clicked!"; 
-}
-// Show a message console when the button is clicked
-function logMessage() { 
-  console.log("Hello, World"); 
-}
-// Show an print when the button is clicked
-function printPage() { 
-  window.print(); 
-}
-// Show an heading and paragraph when the button is clicked
-function myParagraph() {
+=// Change text when the button is clicked
+const changeText = () => {
+  document.getElementById("msg").innerText = "Button clicked!";
+};
+
+// Log a message to the console
+const logMessage = () => {
+  console.log("Hello, World");
+};
+
+// Print the page
+const printPage = () => {
+  window.print();
+};
+
+// Show heading and paragraph
+const showParagraph = () => {
   document.getElementById("demo1").innerHTML = "<ins>Information</ins>";
-  document.getElementById("demo2").innerText = "This is currently under construction";
-}
-/* Count button clicks */
+  document.getElementById("demo2").innerText =
+    "This is currently under construction";
+};
+
+// Count button clicks
 let clicks = 0;
-function addClick() { 
-  clicks += 1; 
+const addClick = () => {
+  clicks++;
   document.getElementById("clicks").innerText = clicks;
-}
-// Show a message console 
+};
+
+// Console info
 const siteName = "Home";
 console.log("Site:", siteName);
-// Show a message console when solve
+
+// Simple calculation
 const count = 1;
 const date = 8;
-let sum = count + date;
+const sum = count + date;
 console.log(sum);
-// Allowed
-{
-let clicks = 2;  
-}
-// Create an Array:
-const subject = ["Math", "Reading", "Science"];
-// Change an element:
-subject[2] = "Class";
-// Add an element:
-subject.push("By Erlin");
-// Display the Array:
-document.getElementById("demo3").innerHTML = subject; 
+
+// Array example
+const subjects = ["Math", "Reading", "Science"];
+
+// Modify array
+subjects[2] = "Class";
+subjects.push("By Erlin");
+
+// Display array
+document.getElementById("demo3").innerText = subjects.join(", ");
+
 
 
