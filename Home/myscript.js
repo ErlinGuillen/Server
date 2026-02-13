d4a6ed8bc835c011ab6172746b68bacdf0bcfcb5
ErlinGuillen <guillenvelasquezerlin@gmail.com>
2026-02-13 14:38:06 -0500

    Update myscript.js

--- Home/myscript.js
+++ Home/myscript.js
@@ -1,5 +1,4 @@
-document.getElementById("demo").innerHTML = "Hello <b>World</b>!";
-document.getElementById("text").innerText = "Hello World!";
-document.write("This text is written to the page");
-window.alert("Hello! This is an alert.");
-console.log("Hello from the console!");
+let count = 0;
+count = count + 1;
+
+const websiteName = "My Website";
