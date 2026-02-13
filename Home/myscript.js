7ed3bbeeb073d987ee7377607a22dc488b6a391b
ErlinGuillen <guillenvelasquezerlin@gmail.com>
2026-02-12 20:12:31 -0500

    Update myscript.js

--- Home/myscript.js
+++ Home/myscript.js
@@ -1,2 +1,13 @@
+// Output to the browser console
+console.log("Hello World!");
 
+// Output using an alert popup
+alert("Welcome to my website!");
 
+// Output directly into the HTML page
+document.write("This text was added by JavaScript.");
+
+// demo
+document.getElementById("message").textContent = "Hello from JavaScript!";
+let variableName = "value";
+console.log(variableName);
