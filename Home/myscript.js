60edd264206ad062a549432fac553e7bdfab4250
ErlinGuillen <guillenvelasquezerlin@gmail.com>
2026-03-06 18:27:28 -0500

    Add files via upload

--- Home/myscript.js
+++ Home/myscript.js
@@ -1,1 +1,19 @@
-document.getElementById("version").innerHTML = "March 2026 - Build 737 - By Erlin Guillen-Velasquez"
+document.getElementById("version").innerHTML = "March 2026 - Build 807 - By Erlin Guillen-Velasquez"
+
+document.getElementById("time").innerHTML = Date()
+
+document.getElementById("code").innerHTML = "These button aren't working";
+
+function mystyle() {
+  document.getElementById("demo").style.fontSize = "25px";
+}
+
+function light(sw) {
+  var pic;
+  if (sw == 0) {
+    pic = "pic_bulboff.gif"
+  } else {
+    pic = "pic_bulbon.gif"
+  }
+  document.getElementById('myImage').src = pic;
+}
