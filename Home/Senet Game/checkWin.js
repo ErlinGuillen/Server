6cce33307e2cc5ee39a829514a48c3d7c6242d63
ErlinGuillen <guillenvelasquezerlin@gmail.com>
2026-03-09 16:06:31 -0400

    Add files via upload

--- Home/Senet Game/checkWin.js
+++ Home/Senet Game/checkWin.js
@@ -1,12 +1,18 @@
-       function checkWin() {
-            const p1Pieces = gameState.filter(x => x === 1).length;
-            const p2Pieces = gameState.filter(x => x === 2).length;
-            
-            if (p1Pieces === 0) {
-                alert("Player 1 Wins the journey to the Afterlife!");
-                resetGame();
-            } else if (p2Pieces === 0) {
-                alert("Player 2 Wins the journey to the Afterlife!");
-                resetGame();
-            }
-        }
+function checkWin() {
+    const p1Pieces = gameState.filter(x => x === 1).length;
+    const p2Pieces = gameState.filter(x => x === 2).length;
+
+    if (p1Pieces === 0) {
+        alert("Player 1 Wins!");
+        p1Wins++;
+        document.getElementById('p1-score').innerText = p1Wins;
+        addToLog("Player 1 won the match!");
+        resetGame();
+    } else if (p2Pieces === 0) {
+        alert("Player 2 Wins!");
+        p2Wins++;
+        document.getElementById('p2-score').innerText = p2Wins;
+        addToLog("Player 2 won the match!");
+        resetGame();
+    }
+}
